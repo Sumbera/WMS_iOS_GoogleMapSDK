ab79c50f6b73c88d679b4e501c6613ead2655ba3
Stanislav <stanislav@sumbera.com>
2015-05-23 21:05:18 +0200

    upfate to xcode 6.1 and google sdk 1.9.2

--- WMSOnGoogleMap/WMSTileLayer.h
+++ WMSOnGoogleMap/WMSTileLayer.h
@@ -84,7 +84,7 @@ NS_INLINE BBox bboxFromXYZ(NSUInteger x, NSUInteger y, NSUInteger z){
 NS_INLINE NSString* md5Hash (NSString* stringData) {
     NSData *data = [stringData dataUsingEncoding:NSUTF8StringEncoding];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5([data bytes], [data length], result);
+    CC_MD5([data bytes], (CC_LONG)[data length], result);
     
     return [NSString stringWithFormat:
             @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
